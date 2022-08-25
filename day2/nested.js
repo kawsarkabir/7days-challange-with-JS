42daefd85be1d68e248824bb8299e5b1e36ec2d3
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 15:42:03 +0600

    Multi stage condition

---
+++ day2/nested.js
@@ -0,0 +1,17 @@
+// (advanced) Multi stage condition and nested conditions
+var money = 100;
+var danishPrice = 220;
+var batterBonPrice = 150;
+var chanachurPrice = 50;
+if(danishPrice < money){
+    console.log("danish khabo");
+}
+else if(batterBonPrice < money ) {
+    console.log("battebOn khabo");
+}
+else if( chanachurPrice < money){
+    console.log("chanachur khabo");
+}
+else{
+    console.log("ajke amr khuda nei");
+}
