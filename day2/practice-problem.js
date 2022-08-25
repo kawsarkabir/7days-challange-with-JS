5c557adc21768e52b6532a10755cccb68b85356e
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-26 01:25:37 +0600

    practice problme 1 sloved

---
+++ day2/practice-problem.js
@@ -0,0 +1,10 @@
+// problem 1
+var fruits = ["apple", "banana","orange"];
+console.log(fruits.indexOf("banana"));
+fruits[1] = "mango"
+console.log(fruits);
+
+// problem 2
+fruits.pop();
+fruits.push("watermelon");
+console.log(fruits);
