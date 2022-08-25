dc98a7c43163aae9f2bc7d75be65133f7d2a5022
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 11:59:16 +0600

    indexOf

---
+++ day2/index.js
@@ -0,0 +1,14 @@
+//  Array index, get and set by index, indexOf
+
+// index
+var model = ["sabana", "esmin", "janu", "manu"];
+console.log(model [0]);
+console.log(model [3]);
+console.log(model[2]);
+model [2] = "baby";
+console.log(model);
+console.log(model[2]);
+
+// indexOf
+console.log(model.indexOf("baby"));
+
