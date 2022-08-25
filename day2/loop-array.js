bd2610283c2a2e5896f85e5cf1a380ef94cfdd7c
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 23:21:39 +0600

    ran loop array

---
+++ day2/loop-array.js
@@ -0,0 +1,15 @@
+// Run a loop for each element of an array
+// terget: display every element of an array 
+
+
+// var numbers = [45, 40, 42, 20, 92, 60, 70 ,30];
+// for(var i = 0; i < numbers.length; i++){
+//     number = numbers[i];
+//     console.log(number);
+// }
+
+var friends = ["anamul", "mubarok", "hridoy", "antor"];
+for(var i = 0; i < friends.length ; i++){
+    friend = friends[i];
+    console.log(friend);
+}
