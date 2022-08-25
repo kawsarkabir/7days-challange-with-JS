10416a0556fc58667437a3fd829d50bf9658600d
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 23:00:03 +0600

    for lopp

---
+++ day2/for.js
@@ -0,0 +1,20 @@
+// For loop, how for loop works, while vs for loop
+
+// var number = 1;
+//  for(var i = 0; i < 100 ; i ++){
+//     console.log(number);
+//  }
+
+ for(var i = 1; i <= 10; i++){
+    console.log(i);
+ }
+
+//  even number 
+for(var i = 0; i<= 100; i += 2){
+    console.log(i);
+}
+
+// odd number 
+for(var i = 1 ; i <= 100; i += 2){
+    console.log(i);
+}
