3487eb2ac04288cb884e3100e0c609bafc2ab7f6
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 23:39:16 +0600

    reversed way in loop

---
+++ day2/loop-break.js
@@ -0,0 +1,12 @@
+var number = 10 ;
+while (number >= 1) {
+    number--;
+    console.log(number);
+}
+
+// for loop
+
+var friends  = ["ami", "tumi", "se",];
+for(var i = 0; i > friends.length; i--){
+    console.log(i);
+}
