7f703bec9130ea5f249202fa8f9fa9b55b72aac0
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 11:44:03 +0600

    array declaration

--- day2/array.js
+++ day2/array.js
@@ -1,1 +1,11 @@
 // Declare Array, array length and array index
+// array declaration
+var janu = [17, 20 , 45, 30, 19, 27];
+console.log(janu);
+
+// string type array
+
+var nayok = ["Hero Alam", "Anonto Jalil", "Patla Khan"];
+console.log(nayok.length);
+// array এর মধ্যে কয়টা উপাদান আছে সেটা জানা যাই
+console.log([0]);
