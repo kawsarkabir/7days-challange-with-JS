09d8cd0f6422fd25d20da9f9c62ebce122bd9afd
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 15:27:35 +0600

    multiple-conditon

---
+++ day2/multiple-condition.js
@@ -0,0 +1,24 @@
+// Handle multiple conditions, and or
+
+// var isGraduate = true;
+// var isSalary = 90000;
+// var isduplaceBari = 1;
+// if (isGraduate == true && isSalary > 50000 && isduplaceBari > 1){
+//     console.log("khala apnar meyere amar loge biye diye den");
+// }
+// else{
+//     console.log("amar r khalatu bonre biya kora lagbo nah");
+// }
+
+
+
+//  or 
+var isNamazi = true;
+var ishonest = false;
+var isCar  = 2
+if(isNamazi == false || ishonest== true || isCar > 5){
+    console.log("tumare amra khujtaci");
+}
+else{
+    console.log("tor kase jorinar meyerew biye dibo na");
+}
