0254ee8954f153cc76c6a22930f8b142bbe1e757
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 15:06:03 +0600

    if-else

---
+++ day2/if-else.js
@@ -0,0 +1,9 @@
+//  Make conditional decision, if-else, comparison
+var iphonePrice = 1800000;
+var myBadge = 30000;
+if ( myBadge >= iphonePrice) {
+    console.log("i phone kinmu");
+}
+else{
+    console.log(" ami goribs vai amar teha poish nai ");
+}
