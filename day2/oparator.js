f5203e5a813be6f3390952fd94be3c548acfbc2b
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 12:52:51 +0600

    comparison

---
+++ day2/oparator.js
@@ -0,0 +1,19 @@
+// Compare variables and Comparison operator
+var result = [4, 5, 6, 7, 8,];
+// 5, 6 ar theke choto 
+var result = 5 < 6 ;
+var result = 6 = 6;
+var result = 6 > 5;
+var result = 6==7;
+var result = 7 != 6;
+
+
+// multiiple condition 
+
+//  money1 > money2 && result1 < result2 
+
+
+// aka na hoi arekta  ||
+
+// result1 > result2 || money1 < money2 ||
+
