9ed360a285dda47a0c1c17e5b1633edbd34ba313
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-25 12:17:56 +0600

    push-pop

---
+++ day2/push-pop.js
@@ -0,0 +1,23 @@
+// Add or remove element from array using push, pop
+// push
+var numbers = [20, 30 , 40 , 50 , 60 , 70 , 80, 90];
+numbers.push(100)
+console.log(numbers);
+
+// pop
+numbers.pop()
+console.log(numbers);
+
+
+var myGf = ["janu", "manu", "sanu", "babu"];
+myGf.push("riya");
+console.log(myGf);
+myGf.pop()
+console.log(myGf);
+
+
+// shift unshift
+myGf.shift()
+console.log(myGf);
+myGf.unshift("sanme ase boso")
+console.log(myGf);
