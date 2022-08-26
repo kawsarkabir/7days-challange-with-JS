dd5e2ad0055c5e399ba8baf52ae4b8f289ceccfa
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-26 09:10:10 +0600

    funtion declaretion

---
+++ day3/declare-funtion.js
@@ -0,0 +1,15 @@
+// Declare a Function, call function, function vs loop
+
+function startFan(){
+    console.log("stand up");
+    console.log("walk up in the morning");
+    console.log("press the switch");
+}
+startFan();
+
+
+function bringSingara(money) {
+    console.log("mama singara den");
+    console.log(money);
+}
+bringSingara(200);
