95a66fe187f5eb59cfb2d6e1c66b6f36bf5dc7a3
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-26 20:27:47 +0600

    funtion sumary

---
+++ day3/funtion-sumari.js
@@ -0,0 +1,17 @@
+// 19-5 Function examples and Function summary
+
+// function getAvarage(assingment1, assingment2, assingment3){
+//     const total = assingment1 + assingment2 +assingment3;
+//     const avarage = total / 3;
+//     return avarage;
+// }
+// const assingment1Marks = 60;
+// const assingment2Marks = 26;
+// const assingment3Marks = 60;
+
+// var myAvarage = getAvarage(assingment1Marks, assingment2Marks, assingment3Marks)
+// console.log(myAvarage);
+
+
+
+ 
