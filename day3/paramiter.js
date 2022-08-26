5663ff851e52f0c0500a5226767ef2ca24ce4223
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-26 09:28:09 +0600

    paramiter

---
+++ day3/paramiter.js
@@ -0,0 +1,13 @@
+// Add Function parameter, handle multiple parameters
+
+function bringSingara(money) {
+    console.log("ai nen singara");
+}
+bringSingara(100);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log("jokFol", sum);
+}
+sum( 30, 45, 72, 89, 67)
