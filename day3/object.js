5a7c48259334d5f97a0699fea681df96e24af434
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-27 00:54:05 +0600

    multiple obaject declare

--- day3/object.js
+++ day3/object.js
@@ -1,9 +1,30 @@
-var mobile = {
-    price: 18000,
-    brand: "samsung",
-    Storage: "48MPX",
-    camera: "esonic"
+// var mobile = {
+//     price: 18000,
+//     brand: "samsung",
+//     Storage: "48MPX",
+//     camera: "esonic"
+// }
+// console.log(mobile);
+// mobile.brand = "lenovo";
+// console.log(mobile);
+
+// multiple ways to get and set object property
+
+var shoppingCart = {
+    books: 3,
+    sunglass: 4,
+    keyboard: 2,
+    pen:10,
 }
-console.log(mobile);
-mobile.brand = "lenovo";
-console.log(mobile);
+console.log(shoppingCart);
+
+// system 1
+shoppingCart.keyboard = 40;
+console.log(shoppingCart);
+
+// system 2
+shoppingCart["books"] = 15;
+console.log(shoppingCart);
+
+// system 3
+ 
