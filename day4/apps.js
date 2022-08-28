0e12a4c6ce8dd77bf5c60f26159db638ab9c1b26
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-28 13:15:46 +0600

    inchTOFeet

---
+++ day4/apps.js
@@ -0,0 +1,25 @@
+// // variable
+// var name = "kawsar kabir"; 
+// console.log(name);
+// // data type 
+// // Number String, Boolean, undefined, null, 
+// // number
+// var number1 = 10;
+// console.log(number1);
+// // bolean
+// var isSingle = true;
+// // 
+
+// var age = 18;
+// if(age >=18){
+//     console.log(" i dite parbo");
+// }
+// else{
+//     console.log("ami baccha manush");
+// }
+
+
+var name = "kawsar kabir"
+for(var i = 0; i <= 10; i++){
+    console.log(name);
+}
---
+++ day4/recup.js
@@ -0,0 +1,16 @@
+// const myInches = 12;
+// const myFeet = myInches / 12
+// console.log(myFeet);
+
+// const dadiInches = 60;
+// const dadiFeet = dadiInches / 12;
+// console.log(dadiFeet);
+
+
+function inchToFeet(inches){
+    const myFeet = inches / 12;
+    return myFeet;
+}
+const dadaInches = 60
+const dadaFeet = inchToFeet(dadaInches);
+console.log(dadaFeet);
