05007465b472ca8d5418625c42d7e1361dab554a
kawsar kabir <kawsarkabir11@gmail.com>
2022-08-28 20:21:44 +0600

    getElementByTagName

--- day4/script.js
+++ day4/script.js
@@ -1,1 +1,6 @@
-console.log("hello Dom");
+//  console.log("Hello Dom");
+// getElementByTagName
+const licollection = document.getElementsByTagName("h1");
+for(const h1 of licollection){
+       console.log(h1.innerText);
+}
